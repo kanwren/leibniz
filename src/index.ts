61141d264f7483eb4cba505caa615b5c48ca4074
nprindle <nprindle18@gmail.com>
2020-08-03 10:23:12 -0400

    Add 'testSub', 'testSuper', 'test', and more documentation

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ export function sym<A, B>(p: Eq<A, B>): Eq<B, A> {
  * Transitivity of Leibniz equality
  */
 export function trans<A, B, C>(_1: Eq<A, B>, _2: Eq<B, C>): Eq<A, C> {
-    return refl as any; // TS isn't smart enough to figure this one out
+    return refl as any; // this function is true, but can't be written
 }
 
 /**
@@ -54,9 +54,10 @@ export function cast<A, B>(p: Eq<A, B>): (x: A) => B {
 }
 
 /**
- * Test that two types are equal. The third type parameter can be used to give a
- * name to the test. Usually, it suffices to pass 'refl' as the only argument;
- * this is needed to instantiate the impredicative type. For example:
+ * Test that two types are equal. The optional third type parameter may be used
+ * to document the purpose of the test. Usually, it suffices to pass 'refl' as
+ * the only argument; this is needed to instantiate the impredicative type. For
+ * example:
  *
  * <code>
  * const x = 3;
@@ -79,9 +80,52 @@ export function cast<A, B>(p: Eq<A, B>): (x: A) => B {
  * If the test fails, useful information about the error should begin at the
  * third line of the type mismatch error message.
  */
-export function testEq<A, B, _ = "">(p: Eq<A, B>): Eq<A, B> {
+export function testEq<A, B, _doc = "">(p: Eq<A, B>): Eq<A, B> {
     return p;
 }
+
+/**
+ * Test that the first type is a subtype (is assignable to) the second type.
+ * Does not require a Leibniz proof of equality. The optional third type
+ * parameter may be used to document the purpose of the test. For example:
+ *
+ * <code>
+ * const x = 3;
+ * testSub<
+ *     typeof x,
+ *     number,
+ *     "const infers broad type of literal or narrower"
+ * >
+ * </code>
+ */
+export function testSub<_ extends B, B, _doc = "">(): void {}
+
+/**
+ * Test that the first type is a supertype (is assignable from) the second type.
+ * Does not require a Leibniz proof of equality. The optional third type
+ * parameter may be used to document the purpose of the test.
+ */
+export function testSuper<A, _ extends A, _doc = "">(): void {}
+
+/**
+ * Group a block of code into a test to verify that the block of code compiles.
+ * Returning an object or array literal can be used to group together multiple
+ * tests. The optional type parameter may be used to document the purpose of the
+ * test. For example:
+ *
+ * <code>
+ * test<"these function calls all compile">(() => [
+ *     foo(3),
+ *     bar("test"),
+ *     baz(foo),
+ * ]);
+ * </code>
+ *
+ * This is no different than just using the code at the top level, but can be
+ * used for logical grouping, conveying intent, and for avoiding
+ * declared-but-not-used warnings.
+ */
+export function test<_doc = "">(_: () => void): void {}
 
 /**
  * Curry a two-argument function. This can be useful when partially applying a
